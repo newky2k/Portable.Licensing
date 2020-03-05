36e0cf94e03e460e04a8d098d4689ff45651d00b
Dave Humphreys <newky2k@noreply.githhub.com>
2020-03-05 15:01:50 +0000

    Added the new .net standard code

---
+++ src/Portable.Licensing/Security/Cryptography/Seeder.cs
@@ -0,0 +1,21 @@
+﻿using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portable.Licensing.Security.Cryptography
+{
+    internal static class Seeder
+    {
+        internal static byte[] GenerateSeed(int seedLength = 32)
+        {
+            var random = new SecureRandom();
+
+            var salt = random.GenerateSeed(seedLength);
+
+            return salt;
+        }
+
+
+    }
+}
